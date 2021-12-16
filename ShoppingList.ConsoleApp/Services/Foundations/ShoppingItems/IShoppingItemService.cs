3cd40995e66f5ca3c5455be78e10d84f31e4e352
Tharani <tharanisantosh@outlook.com>
2021-12-16 06:23:16 +0200

    CODE RUB: Code cleanup

--- ShoppingList.ConsoleApp/Services/Foundations/ShoppingItems/IShoppingItemService.cs
+++ ShoppingList.ConsoleApp/Services/Foundations/ShoppingItems/IShoppingItemService.cs
@@ -2,11 +2,6 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // ------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ShoppingList.ConsoleApp.Models.ShoppingItems;
 
 namespace ShoppingList.ConsoleApp.Services.Foundations.ShoppingItems
