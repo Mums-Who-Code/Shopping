4f854374635aeec96c1c2df5025cdb76deab7ff0
Tharani <tharanisantosh@outlook.com>
2021-12-16 06:22:21 +0200

    BROKER: Add Logging

---
+++ ShoppingList.ConsoleApp/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,24 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace ShoppingList.ConsoleApp.Brokers.Loggings
+{
+    class LoggingBroker : ILoggingBroker
+    {
+        public ILogger logger { get; set; }
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception, exception.Message);
+    }
+}
