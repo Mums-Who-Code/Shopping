524fad95a6a08089d0a9fee77c05fc92d564f3d8
Tharani <tharanisantosh@outlook.com>
2022-01-13 05:04:13 +0200

    ShouldThrowValidationExceptionOnAddIfShoppingItemIsNullAndLogIt -> WIP

---
+++ ShoppingList.ConsoleApp/Models/ShoppingItems/Exceptions/NullShoppingItemException.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace ShoppingList.ConsoleApp.Models.ShoppingItems.Exceptions
+{
+    public class NullShoppingItemException : Xeption
+    {
+        public NullShoppingItemException()
+            : base(message: "Shopping item is null.")
+        {     
+        }
+    }
+}
---
+++ ShoppingList.Tests.Unit/Services/Foundations/ShoppingItems/ShoppingItemServiceTests.Validations.Add.cs
@@ -0,0 +1,33 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using ShoppingList.ConsoleApp.Models.ShoppingItems;
+using ShoppingList.ConsoleApp.Models.ShoppingItems.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShoppingList.Tests.Unit.Services.Foundations.ShoppingItems
+{
+    public partial class ShoppingItemServiceTests
+    {
+        [Fact]
+        public void ShouldThrowValidationExceptionOnAddIfShoppingItemIsNullAndLogIt()
+        {
+            // given
+            ShoppingItem nullShoppingItem = null;
+            var nullShoppingItemException = new NullShoppingItemException();
+            var expectedShoppingItemValidationException = new ShoppingItemValidationException(nullShoppingItemException);
+
+            // when
+            Action addShoppingItemTask = () => this.shoppingItemService.AddShoppingItem(nullShoppingItem);
+
+            // then
+            Assert.Throws<ShoppingItemValidationException>(addShoppingItemTask);
+        }
+    }
+}
