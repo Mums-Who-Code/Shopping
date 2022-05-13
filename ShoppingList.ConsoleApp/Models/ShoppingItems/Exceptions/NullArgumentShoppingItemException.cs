6699c898b9585b5f96c3065202d5701cd0edd8fb
Tharani <tharanisantosh@outlook.com>
2022-05-13 07:24:37 +0200

    ShouldThrowDependencyValidationExceptionOnRetrieveByIdIfValidationErrorOccursAndLogIt -> FAIL

---
+++ ShoppingList.ConsoleApp/Models/ShoppingItems/Exceptions/NullArgumentShoppingItemException.cs
@@ -0,0 +1,17 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace ShoppingList.ConsoleApp.Models.ShoppingItems.Exceptions
+{
+    public class NullArgumentShoppingItemException : Xeption
+    {
+        public NullArgumentShoppingItemException(Exception innerException)
+            : base(message: "Null argument shopping item error occured, fix the errors and try again.",
+                  innerException)
+        { }
+    }
+}
