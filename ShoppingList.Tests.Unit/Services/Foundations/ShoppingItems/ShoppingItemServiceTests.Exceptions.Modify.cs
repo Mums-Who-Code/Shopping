8f404bbf3ae3287cbc5b4a5759ee3596bff377d0
Tharani <tharanisantosh@outlook.com>
2022-05-18 05:02:09 +0200

    ShouldThrowDependencyValidationExceptionOnModifyIfValidationErrorOccursAndLogIt -> PASS

---
+++ ShoppingList.Tests.Unit/Services/Foundations/ShoppingItems/ShoppingItemServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,51 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using Moq;
+using ShoppingList.ConsoleApp.Models.ShoppingItems;
+using ShoppingList.ConsoleApp.Models.ShoppingItems.Exceptions;
+using Xunit;
+
+namespace ShoppingList.Tests.Unit.Services.Foundations.ShoppingItems
+{
+    public partial class ShoppingItemServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyValidationExceptionOnModifyIfValidationErrorOccursAndLogIt()
+        {
+            // given
+            ShoppingItem someShoppingItem = CreateRandomShoppingItem();
+            var argumentNullException = new ArgumentNullException();
+
+            var nullArgumentShoppingItemException =
+                new NullArgumentShoppingItemException(argumentNullException);
+
+            var expectedShoppingItemDependencyValidationException =
+                new ShoppingItemDependencyValidationException(nullArgumentShoppingItemException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdateShoppingItem(It.IsAny<ShoppingItem>()))
+                    .Throws(argumentNullException);
+
+            // when
+            Action modifyShoppingItemAction = () => this.shoppingItemService.ModifyShoppingItem(someShoppingItem);
+
+            // then
+            Assert.Throws<ShoppingItemDependencyValidationException>(modifyShoppingItemAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateShoppingItem(It.IsAny<ShoppingItem>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedShoppingItemDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
