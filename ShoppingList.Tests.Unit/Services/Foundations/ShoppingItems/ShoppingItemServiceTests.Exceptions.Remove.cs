138a278a5c052930eefd1254a82c6a42ac4221be
Tharani <tharanisantosh@outlook.com>
2022-05-19 07:56:01 +0200

    ShouldThrowServiceExceptionOnRemoveIfServiceErrorOccursAndLogIt -> PASS

---
+++ ShoppingList.Tests.Unit/Services/Foundations/ShoppingItems/ShoppingItemServiceTests.Exceptions.Remove.cs
@@ -0,0 +1,51 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using Moq;
+using ShoppingList.ConsoleApp.Models.ShoppingItems;
+using ShoppingList.ConsoleApp.Models.ShoppingItems.Exceptions;
+using Xunit;
+
+namespace ShoppingList.Tests.Unit.Services.Foundations.ShoppingItems
+{
+    public partial class ShoppingItemServiceTests
+    {
+        [Fact]
+        public void ShouldThrowServiceExceptionOnRemoveIfServiceErrorOccursAndLogIt()
+        {
+            // given
+            ShoppingItem someShoppingItem = CreateRandomShoppingItem();
+            var serviceException = new Exception();
+
+            var failedShoppingItemServiceException =
+                new FailedShoppingItemServiceException(serviceException);
+
+            var expectedShoppingItemServiceException =
+                new ShoppingItemServiceException(failedShoppingItemServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.DeleteShoppingItem(It.IsAny<ShoppingItem>()))
+                    .Throws(serviceException);
+
+            // when
+            Action removeShoppingItemAction = () => this.shoppingItemService.RemoveShoppingItem(someShoppingItem);
+
+            // then
+            Assert.Throws<ShoppingItemServiceException>(removeShoppingItemAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteShoppingItem(It.IsAny<ShoppingItem>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedShoppingItemServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
